4443f550d4b1b8b0c938c1df0a3c52ee89ca973b
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-09-14 23:15:09 +0300

    taken time to type is updated in modal and history as well

--- script.js
+++ script.js
@@ -68,7 +68,8 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTakenFraction = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt(timeTakenFraction);
 
   // show result modal
   resultModal.innerHTML = "";
